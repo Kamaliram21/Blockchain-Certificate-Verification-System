5122f3365f99523f5cf2d9294c24bf901deffe44
Kamali <kamaliram719@gmail.com>
2026-07-29 21:49:40 +0530

    Initialize App component for certificate verification

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+function App() {
+
+    return (
+
+        <div>
+
+            <h1>
+                Blockchain Certificate Verification System
+            </h1>
+
+        </div>
+
+    );
+
+}
+
+export default App;
